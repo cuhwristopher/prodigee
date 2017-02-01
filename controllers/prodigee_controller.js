aeab93cc33b2ecb3fec81701088cbf84c608c5f1
Noah Henry Johnson <noah@freenoahj.com>
2017-01-31 20:52:52 -0800

    added jobs controller route

--- controllers/prodigee_controller.js
+++ controllers/prodigee_controller.js
@@ -2,6 +2,11 @@ var db = require("../models");
 
 module.exports = (app) => {
     app.get("/", (request, response) => {
-         response.render("index");
+      db.Jobs.findAll({}).then(function(allJobs){
+        var hbsObject = {
+          jobs: allJobs
+        };
+      })
+         response.render("index", hbsObject);
     });
 };
