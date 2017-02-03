f5da2eb68ca34bf30aa396852224654379f4cdea
Noah Henry Johnson <noah@freenoahj.com>
2017-02-02 19:44:23 -0800

    firebase user creation working.

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var db = require("./models");
 
-
-app.use(express.static(process.cwd() + "/public"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(process.cwd() + "/public"));
 app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
 require("./controllers/prodigee_controller.js")(app);
+require("./controllers/html-routes.js")(app);
 
 db.sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
