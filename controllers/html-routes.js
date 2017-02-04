85a4f4489468580b1ed51f2820669ea41e5d7680
cuhwristopher <Chris-MAC@Christophers-MacBook-Pro.local>
2017-02-03 21:05:36 -0800

    updated routes on user profile page

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -10,13 +10,21 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads signup.html
+  app.get("/", function(req, res) {
+         res.sendFile(path.join(__dirname + "/../public/main.handlebars"));
+     });
+
   app.get("/signup", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/signup.html"));
   });
 
   // add route loads the post-job.html page,
   // where users can enter new characters to the db
-  app.get("/add", function(req, res) {
+  app.get("/post-job", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/post-job.html"));
+  });
+
+  app.get("/create-job", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/create-job.html"));
   });
 };
